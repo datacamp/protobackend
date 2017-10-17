475d3af84f27a8bf14759dadbaa5a41cf0608001
Michael Chow <machow@princeton.edu>
2017-10-17 09:51:00 -0400

    bump minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name='protobackend',
-	version='0.0.2',
+	version='0.1.0',
 	packages=['protobackend'],
         description = 'Generic backend',
         author = 'Michael Chow',
