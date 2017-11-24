d83fb1ab5caa98fa012a63c0f286d3500e409022
Michael Chow <machow@princeton.edu>
2017-11-24 11:33:46 -0500

    bump patch version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name='protobackend',
-	version='0.1.0',
+	version='0.1.1',
 	packages=['protobackend'],
         description = 'Generic backend',
         author = 'Michael Chow',
