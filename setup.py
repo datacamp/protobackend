92bf7ab30ca3f05ee92ed81575947f6e00af5934
Jeroen Hermans <jeroen@datacamp.com>
2018-11-22 20:32:01 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
 	name='protobackend',
-	version='0.2.0',
+	version='0.2.1',
 	packages=['protobackend'],
         description = 'Generic backend',
         author = 'Michael Chow',
